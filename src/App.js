113794be1bbfafd72fc38d0ed9d853aeee031115
DripMicro <whappydrip@gmail.com>
2023-01-31 09:04:05 -0700

    upgrade project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './components/Home';
- import { About } from './components/About';
-import './App.css';
+import Home from "./pages/Home";
+import Support from "./pages/Support";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import WeatherApp from "./pages/Weather";
+import WeatherMain from "./pages/WeatherMain";
+import NotFound from "./pages/404";
+import ForecastWeather from "./pages/ForecastWeather";
+import Settings from "./pages/Settings";
+import { db } from "./backend/app_backend";
+import "./autoload";
 
 function App() {
+  let homePageSeen = db.get("HOME_PAGE_SEEN");
+  let DEFAULT_ROUTE_PAGE;
+  homePageSeen
+    ? (DEFAULT_ROUTE_PAGE = <WeatherApp />)
+    : (DEFAULT_ROUTE_PAGE = <Home />);
+
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={DEFAULT_ROUTE_PAGE} />
+        <Route path="support" element={<Support />} />
+        <Route path="weather" element={<WeatherApp />} />
+        <Route path="weathermain" element={<WeatherMain />} />
+        <Route path="forecast" element={<ForecastWeather />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import  {  BrowserRouter  }  from  'react-router-dom'
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
+const config = {
+  scope: "/",
+  registerOptions: {
+    updateViaCache: "none"
+  },
+  onSuccess: (registration) => {
+    console.log("Service worker registration successful:", registration);
+  },
+  onUpdate: (registration) => {
+    console.log("Service worker update available:", registration);
+  },
+};
+
+
+serviceWorkerRegistration.register(config);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
